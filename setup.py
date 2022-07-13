3f64e11e8d58f386aac2faa43721caa488de2f3e
Inan <>
2022-07-13 12:07:51 +0300

    Added todo and new version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_description():
 
 setuptools.setup(
     name="mongo_drf_endpoint_logger",
-    version="0.0.2",
+    version="0.0.3",
     author="Inan Delibas",
     author_email="inanndelibas@gmail.com",
     description="An API Logger for your Django Rest Framework project with Mongo DB.",
